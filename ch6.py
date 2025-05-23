902442471492c0cc074a313ada2225f5f157952f
sgaeteBSD <419097@bsd48.org>
2025-05-23 12:49:11 -0700

    finished challenge 6

--- ch6.py
+++ ch6.py
@@ -1,10 +1,8 @@
-ch6.py:
-------
  # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Santiago Gaete
+Break Repeating XOR (Set 1 Challenge 6)
+5/23/2025
 """
 
 BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
@@ -12,51 +10,65 @@ CHARS = " !#$%&\()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]\^_`abcdefg
 
 def main():
     inp1 = input("base64 file: ")
-    with open(inp1, "r") as f:
-        content = f.read().strip()
-        
-    KS = 2
-    leastKS = 0
-    leastDis = 100
-    c = B64ToBytes(content)
+    f = open(inp1, 'r')
+    content = f.read()
+    
+    KS = 2 #from 2 to 40
+    leastKS = 0 #init
+    leastDis = 100 #init
+    content = cleanB64(content) #CLEAN OUT NEWLINES & NON-B64
+    c = B64ToBytes(content) #bytes
     
     while (KS <= 40):
+        #chop up into 4 blocks
         c1 = c[0:KS]
         c2 = c[KS:KS*2]
         c3 = c[KS*2:KS*3]
         c4 = c[KS*3:KS*4]
         
+        #average hamming distance
         dis = (compHam(c1,c2) + compHam(c1,c3) + 
                 compHam(c2,c3) + compHam(c1,c4) + 
-                    compHam(c2,c4)+compHam(c3,c4)) / KS
+                    compHam(c2,c4)+compHam(c3,c4)) / KS / 6
         
-        #print(KS, dis)
+        #print for fun
+        print(KS, dis)
         
+        #update leastDis and leastKS if its smallest
         if (dis < leastDis):
             leastDis = dis
             leastKS = KS
-        KS += 1
-        
+        KS += 1 #increment
+    
+    #print final least
+    print(leastKS, leastDis)
+    
+    #pre-transpose blocks of leastKS size
     bc = []
-    for i in range(len(c) // leastKS):
+    for i in range(len(c) // leastKS): #integer division
         if (i > 0):
             bc.extend([c[(leastKS * i):(leastKS * i) + leastKS]])
-        else:
+        else: #for the first case
             bc.extend([c[0:leastKS]])
     
+    #transpose blocks
     tp = []
     for i in range(leastKS):
         block = [b[i] for b in bc if i < len(b)]
         tp.append(block)
     
     key = []
-    for i in range(leastKS):
-        bk, br, hs = scoreConvert(tp[i])
-        key.append(bk)
-    print(bytes(key))
-    decrypted = repeatXOR(c, *key)
-    print(bytesToASCII(decrypted))
+    for i in range(len(tp)):
+        bk, _, _ = scoreConvert(tp[i])
+        key.append(bk) #add found key character
     
+    print()
+    print(key)
+    print(bytesToASCII(key)) #key found to be Terminator X: Bring the noise
+    print()
+    
+    decrypt = repeatXOR(c, *key)
+    print(bytesToASCII(decrypt))
     
 def compHam(h1, h2):
     inc = 0
@@ -66,7 +78,14 @@ def compHam(h1, h2):
             inc += xor & 1
             xor >>= 1
     return inc
-            
+           
+def cleanB64(b64):
+    cleaned = ''
+    for c in b64:
+        if c in BASE64 or c == '=':
+            cleaned += c
+    return cleaned
+    
 def B64ToBytes(b64):
     byts = bytearray()
     i = 0
@@ -86,21 +105,18 @@ def B64ToBytes(b64):
                 val = 0
             else:
                 chars += 1
+                val = 0
                 for k in range(64):
                     if (BASE64[k] == c):
                         val = k
                         break
             bits |= val << (18 - 6 * j)
+            
         for j in range (chars - 1):
             byte = (bits >> (16 - 8 * j)) & 0xFF
             byts.append(byte)
-    return byts
-    
-def hexToBytes(hx):
-    return list(bytes.fromhex(hx))
-
-def bytesToHex(byts):
-    return (bytes(byts)).hex()
+            
+    return bytes(byts)
     
 def bytesToASCII(byts):
     return bytes(byts).decode("ascii", errors="ignore")
@@ -151,71 +167,3 @@ def singleXOR(data, key):
     return bytes([b ^ key for b in data])
 
 main()
-
-6.txt:
------
-HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS
-BgBHVBwNRU0HBAxTEjwMHghJGgkRTxRMIRpHKwAFHUdZEQQJAGQmB1MANxYG
-DBoXQR0BUlQwXwAgEwoFR08SSAhFTmU+Fgk4RQYFCBpGB08fWXh+amI2DB0P
-QQ1IBlUaGwAdQnQEHgFJGgkRAlJ6f0kASDoAGhNJGk9FSA8dDVMEOgFSGQEL
-QRMGAEwxX1NiFQYHCQdUCxdBFBZJeTM1CxsBBQ9GB08dTnhOSCdSBAcMRVhI
-CEEATyBUCHQLHRlJAgAOFlwAUjBpZR9JAgJUAAELB04CEFMBJhAVTQIHAh9P
-G054MGk2UgoBCVQGBwlTTgIQUwg7EAYFSQ8PEE87ADpfRyscSWQzT1QCEFMa
-TwUWEXQMBk0PAg4DQ1JMPU4ALwtJDQhOFw0VVB1PDhxFXigLTRkBEgcKVVN4
-Tk9iBgELR1MdDAAAFwoFHww6Ql5NLgFBIg4cSTRWQWI1Bk9HKn47CE8BGwFT
-QjcEBx4MThUcDgYHKxpUKhdJGQZZVCFFVwcDBVMHMUV4LAcKQR0JUlk3TwAm
-HQdJEwATARNFTg5JFwQ5C15NHQYEGk94dzBDADsdHE4UVBUaDE5JTwgHRTkA
-Umc6AUETCgYAN1xGYlUKDxJTEUgsAA0ABwcXOwlSGQELQQcbE0c9GioWGgwc
-AgcHSAtPTgsAABY9C1VNCAINGxgXRHgwaWUfSQcJABkRRU8ZAUkDDTUWF01j
-OgkRTxVJKlZJJwFJHQYADUgRSAsWSR8KIgBSAAxOABoLUlQwW1RiGxpOCEtU
-YiROCk8gUwY1C1IJCAACEU8QRSxORTBSHQYGTlQJC1lOBAAXRTpCUh0FDxhU
-ZXhzLFtHJ1JbTkoNVDEAQU4bARZFOwsXTRAPRlQYE042WwAuGxoaAk5UHAoA
-ZCYdVBZ0ChQLSQMYVAcXQTwaUy1SBQsTAAAAAAAMCggHRSQJExRJGgkGAAdH
-MBoqER1JJ0dDFQZFRhsBAlMMIEUHHUkPDxBPH0EzXwArBkkdCFUaDEVHAQAN
-U29lSEBAWk44G09fDXhxTi0RAk4ITlQbCk0LTx4cCjBFeCsGHEETAB1EeFZV
-IRlFTi4AGAEORU4CEFMXPBwfCBpOAAAdHUMxVVUxUmM9ElARGgZBAg4PAQQz
-DB4EGhoIFwoKUDFbTCsWBg0OTwEbRSonSARTBDpFFwsPCwIATxNOPBpUKhMd
-Th5PAUgGQQBPCxYRdG87TQoPD1QbE0s9GkFiFAUXR0cdGgkADwENUwg1DhdN
-AQsTVBgXVHYaKkg7TgNHTB0DAAA9DgQACjpFX0BJPQAZHB1OeE5PYjYMAg5M
-FQBFKjoHDAEAcxZSAwZOBREBC0k2HQxiKwYbR0MVBkVUHBZJBwp0DRMDDk5r
-NhoGACFVVWUeBU4MRREYRVQcFgAdQnQRHU0OCxVUAgsAK05ZLhdJZChWERpF
-QQALSRwTMRdeTRkcABcbG0M9Gk0jGQwdR1ARGgNFDRtJeSchEVIDBhpBHQlS
-WTdPBzAXSQ9HTBsJA0UcQUl5bw0KB0oFAkETCgYANlVXKhcbC0sAGgdFUAIO
-ChZJdAsdTR0HDBFDUk43GkcrAAUdRyonBwpOTkJEUyo8RR8USSkOEENSSDdX
-RSAdDRdLAA0HEAAeHQYRBDYJC00MDxVUZSFQOV1IJwYdB0dXHRwNAA9PGgMK
-OwtTTSoBDBFPHU54W04mUhoPHgAdHEQAZGU/OjV6RSQMBwcNGA5SaTtfADsX
-GUJHWREYSQAnSARTBjsIGwNOTgkVHRYANFNLJ1IIThVIHQYKAGQmBwcKLAwR
-DB0HDxNPAU94Q083UhoaBkcTDRcAAgYCFkU1RQUEBwFBfjwdAChPTikBSR0T
-TwRIEVIXBgcURTULFk0OBxMYTwFUN0oAIQAQBwkHVGIzQQAGBR8EdCwRCEkH
-ElQcF0w0U05lUggAAwANBxAAHgoGAwkxRRMfDE4DARYbTn8aKmUxCBsURVQf
-DVlOGwEWRTIXFwwCHUEVHRcAMlVDKRsHSUdMHQMAAC0dCAkcdCIeGAxOazkA
-BEk2HQAjHA1OAFIbBxNJAEhJBxctDBwKSRoOVBwbTj8aQS4dBwlHKjUECQAa
-BxscEDMNUhkBC0ETBxdULFUAJQAGARFJGk9FVAYGGlMNMRcXTRoBDxNPeG43
-TQA7HRxJFUVUCQhBFAoNUwctRQYFDE43PT9SUDdJUydcSWRtcwANFVAHAU5T
-FjtFGgwbCkEYBhlFeFsABRcbAwZOVCYEWgdPYyARNRcGAQwKQRYWUlQwXwAg
-ExoLFAAcARFUBwFOUwImCgcDDU5rIAcXUj0dU2IcBk4TUh0YFUkASEkcC3QI
-GwMMQkE9SB8AMk9TNlIOCxNUHQZCAAoAHh1FXjYCDBsFABkOBkk7FgALVQRO
-D0EaDwxOSU8dGgI8EVIBAAUEVA5SRjlUQTYbCk5teRsdRVQcDhkDADBFHwhJ
-AQ8XClJBNl4AC1IdBghVEwARABoHCAdFXjwdGEkDCBMHBgAwW1YnUgAaRyon
-B0VTGgoZUwE7EhxNCAAFVAMXTjwaTSdSEAESUlQNBFJOZU5LXHQMHE0EF0EA
-Bh9FeRp5LQdFTkAZREgMU04CEFMcMQQAQ0lkay0ABwcqXwA1FwgFAk4dBkIA
-CA4aB0l0PD1MSQ8PEE87ADtbTmIGDAILAB0cRSo3ABwBRTYKFhROHUETCgZU
-MVQHYhoGGksABwdJAB0ASTpFNwQcTRoDBBgDUkksGioRHUkKCE5THEVCC08E
-EgF0BBwJSQoOGkgGADpfADETDU5tBzcJEFMLTx0bAHQJCx8ADRJUDRdMN1RH
-YgYGTi5jMURFeQEaSRAEOkURDAUCQRkKUmQ5XgBIKwYbQFIRSBVJGgwBGgtz
-RRNNDwcVWE8BT3hJVCcCSQwGQx9IBE4KTwwdASEXF01jIgQATwZIPRpXKwYK
-BkdEGwsRTxxDSToGMUlSCQZOFRwKUkQ5VEMnUh0BR0MBGgAAZDwGUwY7CBdN
-HB5BFwMdUz0aQSwWSQoITlMcRUILTxoCEDUXF01jNw4BTwVBNlRBYhAIGhNM
-EUgIRU5CRFMkOhwGBAQLTVQOHFkvUkUwF0lkbXkbHUVUBgAcFA0gRQYFCBpB
-PU8FQSsaVycTAkJHYhsRSQAXABxUFzFFFggICkEDHR1OPxoqER1JDQhNEUgK
-TkJPDAUAJhwQAg0XQRUBFgArU04lUh0GDlNUGwpOCU9jeTY1HFJARE4xGA4L
-ACxSQTZSDxsJSw1ICFUdBgpTNjUcXk0OAUEDBxtUPRpCLQtFTgBPVB8NSRoK
-SREKLUUVAklkERgOCwAsUkE2Ug8bCUsNSAhVHQYKUyI7RQUFABoEVA0dWXQa
-Ry1SHgYOVBFIB08XQ0kUCnRvPgwQTgUbGBwAOVREYhAGAQBJEUgETgpPGR8E
-LUUGBQgaQRIaHEshGk03AQANR1QdBAkAFwAcUwE9AFxNY2QxGA4LACxSQTZS
-DxsJSw1ICFUdBgpTJjsIF00GAE1ULB1NPRpPLF5JAgJUVAUAAAYKCAFFXjUe
-DBBOFRwOBgA+T04pC0kDElMdC0VXBgYdFkU2CgtNEAEUVBwTWXhTVG5SGg8e
-AB0cRSo+AwgKRSANExlJCBQaBAsANU9TKxFJL0dMHRwRTAtPBRwQMAAATQcB
-FlRlIkw5QwA2GggaR0YBBg5ZTgIcAAw3SVIaAQcVEU8QTyEaYy0fDE4ITlhI
-Jk8DCkkcC3hFMQIEC0EbAVIqCFZBO1IdBgZUVA4QTgUWSR4QJwwRTWM=
-
